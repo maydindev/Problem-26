bf3f31b187f6367bb549d5526e09228a65a182c7
maydindev <maydin.dev@outlook.com>
2024-08-27 12:10:04 +0300

    "Added state and effect for stopwatch functionality, replaced console log with seconds counter, and implemented start/stop buttons"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import { useEffect,useState } from "react"
+
+
 export default function Stopwatch() {
-  const timerId = setInterval(() => console.log('count'), 1000)
-  clearInterval(timerId)
+  const [isRunning,setIsRunning] = useState(false)
+  const [seconds,setSeconds] = useState(0)
+
+  useEffect(() => {
+    let timerId
+    if(isRunning){
+      timerId = setInterval(() => {
+        setSeconds(prev => prev + 1)
+        //console.log('count')
+      }, 1000)
+    }  
+    return () => clearInterval(timerId)
+  },[isRunning])
+
+  const handleClickStart = () => {
+    setIsRunning(true)
+  }
+
+  const handleClickStop = () => {
+    setIsRunning(false)
+  }
 
   return (
     <div className='p-12 mx-auto space-y-4 max-w-[300px]'>
-      <div className='font-bold text-center text-3xl'>Zamanlayıcı: 0s</div>
+      <div className='font-bold text-center text-3xl'>Zamanlayıcı: {seconds}s</div>
       <div className='flex justify-between'>
-        <button className='text-amber-500 font-bold'>Durdur</button>
-        <button className='text-green-500 font-bold'>Başlat</button>
+        <button className='text-amber-500 font-bold' onClick={handleClickStop}>Durdur</button>
+        <button className='text-green-500 font-bold' onClick={handleClickStart}>Başlat</button>
       </div>
     </div>
   )
